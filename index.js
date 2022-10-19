57d7971d0db7c3d7fc37b22065702dc28d22e3d4
Nicolas Silva <nicosilva1992@hotmail.com>
2022-10-19 09:43:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+
+const count = 0
+
+const PORT = process.env.PORT || 8080
+
+app.get('/',(req,res)=>{
+    res.send('ok') 
+})
+
+app.get('/count',(req,res)=>{
+    count ++
+    res.send(`han llegado ${count} peticiones`)
+})
+
+server = app.listen(PORT,()=>{
+    console.log('Server runing..');
+})
+server.on('error',e=>{
+    console.log(e);
+})
